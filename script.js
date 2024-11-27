21b6882bb21d77a82ab1d952f736604bdf744b12
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-27 17:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 class Rectangle {
 	constructor(width, height) {
-		this.width = width;
-		this.height = height;
+		this._width = width;
+		this._height = height;
 	}
 
-	// Getter for area
-	get getArea() {
-		return this.width * this.height;
+	// Getter for width
+	get width() {
+		return this._width;
+	}
+
+	// Getter for height
+	get height() {
+		return this._height;
+	}
+
+	// Method to calculate the area
+	getArea() {
+		return this._width * this._height;
 	}
 }
 
@@ -21,7 +31,6 @@ class Square extends Rectangle {
 		return 4 * this.width;
 	}
 }
-
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
