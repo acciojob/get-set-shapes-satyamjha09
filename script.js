67eb22fa153bd34b47b5dd1c396cadff6b186b43
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-27 17:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+		this.width = width;
+		this.height = height;
+	}
 
-class Square extends Animal {}
+	// Getter for area
+	get getArea() {
+		return this.width * this.height;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side) {
+		// A square is a rectangle with equal width and height
+		super(side, side);
+	}
+
+	// Method to calculate the perimeter
+	getPerimeter() {
+		return 4 * this.width;
+	}
+}
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
